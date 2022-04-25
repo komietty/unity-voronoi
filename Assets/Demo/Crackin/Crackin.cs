d77c3923694aacce7dd890e86a7fd7dcef2f65ab
komietty <komietty@gmail.com>
2022-04-26 02:30:10 +0900

    some refactor

--- Assets/Demo/Crackin/Crackin.cs
+++ Assets/Demo/Crackin/Crackin.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Unity.Mathematics;
 using kmty.geom.d3.delauney;
 using kmty.geom.csg;
+using System.Linq;
 
 namespace kmty.geom.crackin {
     using VG = VoronoiGraph3D;
@@ -14,7 +15,7 @@ public class Crackin : MonoBehaviour {
         [SerializeField] protected Material mat;
         [SerializeField] protected int numPoint;
         [SerializeField, Range(-1, 100)] protected int targetID;
-        [SerializeField, Range(0.1f, 2f)] float scale = 1f;
+        [SerializeField, Range(0.1f, 10f)] float scale = 1f;
 
         public Mesh mesh;
         public string BakePath;
@@ -34,7 +35,8 @@ public class Crackin : MonoBehaviour {
                     var f = g.AddComponent<MeshFilter>();
                     var r = g.AddComponent<MeshRenderer>();
                     r.sharedMaterial = mat;
-                    f.mesh = n.Value.mesh;
+                    //f.mesh = n.Value.mesh;
+                    f.mesh = Weld(n.Value.mesh);
                     g.transform.position = (float3)n.Value.center;
                     g.transform.SetParent(this.transform);
                     meshes.Add(g.transform);
@@ -46,7 +48,8 @@ public class Crackin : MonoBehaviour {
 
             var t1 = CSG.GenCsgTree(tgt.transform);
             for (var i = 0; i < meshes.Count; i++) {
-                var t2 = CSG.GenCsgTree(meshes[i], true);
+                //var t2 = CSG.GenCsgTree(meshes[i], true);
+                var t2 = CSG.GenCsgTree(meshes[i]);
                 var o = CSG.Meshing(t1.Oparation(t2, op1));
                 var g = new GameObject(i.ToString());
                 var f = g.AddComponent<MeshFilter>();
@@ -73,6 +76,33 @@ public class Crackin : MonoBehaviour {
 
         void OnDrawGizmos(){
             Gizmos.DrawWireCube(Vector3.one * scale / 2, Vector3.one * scale);
+        }
+
+        Mesh Weld(Mesh original) {
+            var ogl_vrts = original.vertices;
+            var ogl_idcs = original.triangles;
+            var alt_mesh = new Mesh();
+            var alt_vrts = ogl_vrts.Distinct().ToArray();
+            var alt_idcs = new int[ogl_idcs.Length];
+            var vrt_rplc = new int[ogl_vrts.Length];
+            for (var i = 0; i < ogl_vrts.Length; i++) {
+                var o = -1;
+                for (var j = 0; j < alt_vrts.Length; j++) {
+                    //if (alt_vrts[j] == ogl_vrts[i]) { o = j; break; }
+                    if (Vector3.Distance(alt_vrts[j], ogl_vrts[i]) < 0.01f) { o = j; break; }
+                }
+                vrt_rplc[i] = o;
+            }
+
+            for (var i = 0; i < alt_idcs.Length; i++) {
+                alt_idcs[i] = vrt_rplc[ogl_idcs[i]];
+            }
+            alt_mesh.SetVertices(alt_vrts);
+            alt_mesh.SetTriangles(alt_idcs, 0);
+            alt_mesh.RecalculateBounds();
+            alt_mesh.RecalculateNormals();
+            alt_mesh.RecalculateTangents();
+            return alt_mesh;
         }
 
     }
